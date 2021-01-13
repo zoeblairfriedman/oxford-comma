d2f8010deb637f108509defeeba298acb7c61eed
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-13 12:13:38 -0500

    commit tree af901a9db30e6dc5f02a7c2a91203ef09f0584b1

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
+def oxford_comma(array)    
+    if array.length == 1
+      array.join
+    elsif array.length == 2
+      array.join(" and ")
+    else
+      string = String.new
+      string = array[0]
+      i = 1
+      while (i < array.length - 1)  
+        string << ", " + array[i]
+        i+=1  
+      end
+      string << ", and " + array[i]
+      return string
+    end
+    end
 
-end
+
+    
